6ce43c8fb140d12b74f50bbec446a27bf823791f
haicode222 <lenh5k3@gmail.com>
2025-12-10 11:13:19 +0700

    an trom sql

--- HotelBooking_Web/Controllers/RoomsController.cs
+++ HotelBooking_Web/Controllers/RoomsController.cs
@@ -1,35 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace HotelBooking_Web.Controllers
-{
-    using System;
+﻿    using System;
     using System.Collections.Generic;
     using System.Linq;
     using System.Web;
     using System.Web.Mvc;
+    using HotelBooking_Web.Models;
 
-    namespace HotelBooking_Web.Controllers
-    {
+namespace HotelBooking_Web.Controllers
+{
         public class RoomsController : Controller
         {
+                private DataClasses1DataContext db = new DataClasses1DataContext(System.Configuration.ConfigurationManager.ConnectionStrings["QLKSLenh"].ConnectionString);
+            // GET: Rooms
+
             public ActionResult SearchRooms(DateTime? checkin, DateTime? checkout, int? guests)
             {
-                //ViewBag.CheckinDate = checkin;
-                //ViewBag.CheckoutDate = checkout;
-                //ViewBag.GuestCount = guests;
+                    if (checkin == null) checkin = DateTime.Now;
+                    if (checkout == null) checkout = DateTime.Now.AddDays(1);
+                    if (guests == null) guests = 1;
 
-                return View();
-            }
+                    ViewBag.CheckIn = checkin.Value.ToString("yyyy-MM-dd");
+                    ViewBag.CheckOut = checkout.Value.ToString("yyyy-MM-dd");
+                    ViewBag.Guests = guests;
 
-            public ActionResult Detail()
-            {
-                return View();
+                    // Gọi Procedure
+                    var danhSachPhong = db.sp_TimPhongTrong(checkin, checkout, guests).ToList();
+
+                    return View(danhSachPhong);
             }
+            
+
+                public ActionResult Detail(int id)
+                {
+                    var Phong= db.tbl_Phongs.FirstOrDefault(p => p.PhongID == id);
+                    if (Phong == null)
+                    {
+                        return HttpNotFound();
+                    }
+                            return View(Phong);
+                }
 
            
         }
-    }
 }
