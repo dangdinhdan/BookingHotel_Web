c5943f962e2047f108fdfa1aa52d3648478db686
haicode222 <lenh5k3@gmail.com>
2025-12-12 20:15:33 +0700

    8h15

--- HotelBooking_Web/Controllers/RoomsController.cs
+++ HotelBooking_Web/Controllers/RoomsController.cs
@@ -28,19 +28,53 @@ namespace HotelBooking_Web.Controllers
 
                     return View(danhSachPhong);
             }
-            
 
-                public ActionResult Detail(int id)
+            public ActionResult Detail(int id)
+            {
+                ViewBag.PhongID = id;
+                return View();
+            }
+
+        [HttpGet]
+            public JsonResult GetRoomDetailJson(int id) // 'id'
+            {
+                try
                 {
-                    var Phong= db.tbl_Phongs.FirstOrDefault(p => p.PhongID == id);
-                    if (Phong == null)
-                    {
-                        return HttpNotFound();
-                    }
-                            return View(Phong);
-                }
+                    DataClasses1DataContext db = new DataClasses1DataContext();
+                    db.DeferredLoadingEnabled = false;
 
-           
+                    // Tìm phòng theo PhongID (Kiểu int)
+                    var room = db.tbl_Phongs.FirstOrDefault(x => x.PhongID == id);
+
+                    if (room == null) return Json(new { success = false }, JsonRequestBehavior.AllowGet);
+
+                    var listImages = room.tbl_PhongImages.Select(img => img.Url).ToList();
+
+                    // Nếu không có ảnh phụ, lấy ảnh chính
+                    if (listImages.Count == 0 && !string.IsNullOrEmpty(room.HinhAnh))
+                    {
+                        listImages.Add(room.HinhAnh);
+                    }
+                    // Nếu vẫn không có, lấy ảnh placeholder
+                    if (listImages.Count == 0) listImages.Add("/Images/room-placeholder.png");
+
+                    var data = new
+                    {
+                        success = true,
+                        TenPhong = room.SoPhong,
+                        LoaiPhong = room.tbl_LoaiPhong.TenLoaiPhong,
+                        Gia = room.GiaMoiDem,
+                        MoTa = room.MoTa,
+                        DanhSachAnh = listImages
+                    };
+
+                    return Json(data, JsonRequestBehavior.AllowGet);
+            }
+                catch (Exception ex)
+                {
+                    return Json(new { success = false, message = "Lỗi Server: " + ex.Message }, JsonRequestBehavior.AllowGet);
+                }
+            }
         }
 
 }
